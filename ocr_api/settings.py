b55e7e51f484091e6520fc1eba1d3f859961dcde
luochujian <350481693@qq.com>
2017-03-29 10:34:17 +0800

    修复调试页面bug

--- ocr_api/settings.py
+++ ocr_api/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ocr_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR+"/templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
